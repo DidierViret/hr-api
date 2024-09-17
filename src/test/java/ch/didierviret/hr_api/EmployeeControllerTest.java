4316dcd9197dc55a35c6b2b4a088693253147c55
Didier Viret <didier.viret@orif.ch>
2024-09-17 16:51:46 +0200

    Fix warning in testing

--- src/test/java/ch/didierviret/hr_api/EmployeeControllerTest.java
+++ src/test/java/ch/didierviret/hr_api/EmployeeControllerTest.java
@@ -1,6 +1,5 @@
 package ch.didierviret.hr_api;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
