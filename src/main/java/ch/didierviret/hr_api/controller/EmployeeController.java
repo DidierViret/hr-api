d94b07a067658bd4595fc882ba8f9aefdd37f6de
Didier Viret <didier.viret@orif.ch>
2024-09-25 16:05:47 +0200

    Set a deployable configuration for local and Jelastic environment

--- src/main/java/ch/didierviret/hr_api/controller/EmployeeController.java
+++ src/main/java/ch/didierviret/hr_api/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package ch.didierviret.hr_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,12 @@ import ch.didierviret.hr_api.service.EmployeeService;
  */
 @RestController
 public class EmployeeController {
-
+    
     @Autowired
     private EmployeeService employeeService;
+
+    @Autowired
+    private Environment environment;
 
     /**
      * Read - Get all employees
@@ -24,5 +28,17 @@ public class EmployeeController {
     @GetMapping("/employees")
     public Iterable<Employee> getEmployees() {
         return employeeService.getEmployees();
+    }
+
+    /**
+     * Return some text informations to show that the application is running
+     * and to see the value of some environment variables.
+     * 
+     * @return - A String
+     */
+    @GetMapping("/")
+    public String getHello() {
+        return "Hello World >> " + environment.getProperty("name") + " >> " + environment.getProperty("JAVA_HOME") + 
+            " >> " + environment.getProperty("spring.profiles.active") + "<br>" + environment.getProperty("spring.datasource.url");
     }
 }
