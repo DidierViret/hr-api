04256f1413800cfae31b1de703baba1df45ac812
Didier Viret <didier.viret@orif.ch>
2024-11-05 10:53:56 +0100

    Do not duplicate entries in employees table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO employees (first_name, last_name, mail, password) VALUES
-  ('Laurent', 'GINA', 'laurentgina@mail.com', 'laurent'),
-  ('Sophie', 'FONCEK', 'sophiefoncek@mail.com', 'sophie'),
-  ('Agathe', 'FEELING', 'agathefeeling@mail.com', 'agathe');
+INSERT IGNORE INTO employees (id, first_name, last_name, mail, password) VALUES
+  (1, 'Laurent', 'GINA', 'laurentgina@mail.com', 'laurent'),
+  (2, 'Sophie', 'FONCEK', 'sophiefoncek@mail.com', 'sophie'),
+  (3, 'Agathe', 'FEELING', 'agathefeeling@mail.com', 'agathe');
